1f3f1423e1921bde8c5d690df3156ef19ea6271a
FoolLin <theworldrudo549@gmail.com>
2023-08-11 14:56:51 +0700

    Add ScrollContainer parameter
    There are some cases where the ReorderableContainer won't be placed under the ScrollContainer. If this is the case, it can be manually specified here.

--- addons/ReorderableContainer/reorderable_container.gd
+++ addons/ReorderableContainer/reorderable_container.gd
@@ -44,6 +44,11 @@ func set_vertical(value):
 	is_vertical = value
 	_on_sort_children()
 
+## (Optional) [ScrollContainer] refference. Normally, the addon will automatically check 
+## its parent node for [ScrollContainer]. If this is not the case, you can manually specify it here.
+@export
+var scroll_container: ScrollContainer
+
 ## The maximum speed of auto scroll.
 @export 
 var auto_scroll_speed := 10.0
@@ -60,7 +65,6 @@ var auto_scroll_range := 0.3
 var scroll_threshold := 30
 
 
-var _scroll_container: ScrollContainer
 var _scroll_starting_point := 0
 var _is_smooth_scroll := false
 
@@ -76,10 +80,12 @@ var _is_using_process := false
 
 
 func _ready():
-	if get_parent() is ScrollContainer:
-		_scroll_container = get_parent()
-		if _scroll_container.has_method("handle_overdrag"):
-			_is_smooth_scroll = true
+	if scroll_container == null and get_parent() is ScrollContainer:
+		scroll_container = get_parent()
+		
+	if scroll_container != null and scroll_container.has_method("handle_overdrag"):
+		_is_smooth_scroll = true
+	
 	process_mode = Node.PROCESS_MODE_PAUSABLE
 	_adjust_expected_child_rect()
 	if not sort_children.is_connected(_on_sort_children):
@@ -111,7 +117,7 @@ func _process(delta):
 	
 	if _is_hold:
 		_handle_dragging_child_pos(delta)
-		if _scroll_container != null:
+		if scroll_container != null:
 			_handle_auto_scroll(delta)
 	elif not _is_hold and _drop_zone_index != -1:
 		_on_stop_dragging()
@@ -121,8 +127,8 @@ func _process(delta):
 
 
 func _handle_input(delta): 
-	if _scroll_container != null and _is_press and not _is_hold:
-		var scroll_point = _scroll_container.scroll_vertical if is_vertical else _scroll_container.scroll_vertical
+	if scroll_container != null and _is_press and not _is_hold:
+		var scroll_point = scroll_container.scroll_vertical if is_vertical else scroll_container.scroll_vertical
 		if _current_duration == 0:
 			_scroll_starting_point = scroll_point
 		else:
@@ -137,7 +143,7 @@ func _on_start_dragging():
 	_focus_child.z_index = 1
 	# Workaround for SmoothScroll addon
 	if _is_smooth_scroll:
-		_scroll_container.process_mode = Node.PROCESS_MODE_DISABLED	
+		scroll_container.process_mode = Node.PROCESS_MODE_DISABLED	
 
 
 func _on_stop_dragging():
@@ -147,8 +153,8 @@ func _on_stop_dragging():
 	_drop_zone_index = -1
 	reordered.emit(_get_visible_children())
 	if _is_smooth_scroll:
-		_scroll_container.pos = Vector2(_scroll_container.scroll_horizontal, -_scroll_container.scroll_vertical)
-		_scroll_container.process_mode = Node.PROCESS_MODE_INHERIT	
+		scroll_container.pos = Vector2(scroll_container.scroll_horizontal, -scroll_container.scroll_vertical)
+		scroll_container.process_mode = Node.PROCESS_MODE_INHERIT	
 
 
 func _on_node_added(node):
@@ -178,31 +184,31 @@ func _handle_dragging_child_pos(delta):
 
 func _handle_auto_scroll(delta):
 	var mouse_g_pos = get_global_mouse_position()
-	var scroll_g_rect = _scroll_container.get_global_rect()
+	var scroll_g_rect = scroll_container.get_global_rect()
 	if is_vertical:
 		var upper = scroll_g_rect.position.y + (scroll_g_rect.size.y * auto_scroll_range)
 		var lower = scroll_g_rect.position.y + (scroll_g_rect.size.y * (1.0 - auto_scroll_range))
 		
 		if upper > mouse_g_pos.y:
 			var factor = (upper - mouse_g_pos.y) / (upper - scroll_g_rect.position.y)
-			_scroll_container.scroll_vertical -= delta * float(auto_scroll_speed) * 150.0 * factor
+			scroll_container.scroll_vertical -= delta * float(auto_scroll_speed) * 150.0 * factor
 		elif lower < mouse_g_pos.y:
 			var factor = (mouse_g_pos.y - lower) / (scroll_g_rect.end.y - lower)
-			_scroll_container.scroll_vertical += delta * float(auto_scroll_speed) * 150.0 * factor
+			scroll_container.scroll_vertical += delta * float(auto_scroll_speed) * 150.0 * factor
 		else:
-			_scroll_container.scroll_vertical = _scroll_container.scroll_vertical
+			scroll_container.scroll_vertical = scroll_container.scroll_vertical
 	else:
 		var left = scroll_g_rect.position.x + (scroll_g_rect.size.x * auto_scroll_range)
 		var right = scroll_g_rect.position.x + (scroll_g_rect.size.x * (1.0 - auto_scroll_range))
 		
 		if left > mouse_g_pos.x:
 			var factor = (left - mouse_g_pos.x) / (left - scroll_g_rect.position.x)
-			_scroll_container.scroll_horizontal -= delta * float(auto_scroll_speed) * 150.0 * factor
+			scroll_container.scroll_horizontal -= delta * float(auto_scroll_speed) * 150.0 * factor
 		elif right < mouse_g_pos.x:
 			var factor = (mouse_g_pos.x - right) / (scroll_g_rect.end.x - right)
-			_scroll_container.scroll_horizontal += delta * float(auto_scroll_speed) * 150.0 * factor
+			scroll_container.scroll_horizontal += delta * float(auto_scroll_speed) * 150.0 * factor
 		else:
-			_scroll_container.scroll_horizontal = _scroll_container.scroll_horizontal		
+			scroll_container.scroll_horizontal = scroll_container.scroll_horizontal		
 
 
 func _on_sort_children(delta := -1.0):
